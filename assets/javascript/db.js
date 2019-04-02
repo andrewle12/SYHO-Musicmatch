174c9d6cd8aeb9f1f51b6eb0bf9b0c7490a2314a
Charos12 <andrew.le12@gmail.com>
2019-04-02 15:55:13 -0400

    favorites bug

--- assets/javascript/db.js
+++ assets/javascript/db.js
@@ -1,73 +1,73 @@
 var config = {
-    apiKey: "###",
-    authDomain: "musicmatch-api.firebaseapp.com",
-    databaseURL: "https://musicmatch-api.firebaseio.com",
-    projectId: "musicmatch-api",
-    storageBucket: "",
-    messagingSenderId: "970010529438"
+  apiKey: "###",
+  authDomain: "musicmatch-api.firebaseapp.com",
+  databaseURL: "https://musicmatch-api.firebaseio.com",
+  projectId: "musicmatch-api",
+  storageBucket: "",
+  messagingSenderId: "970010529438"
 };
 firebase.initializeApp(config);
 
 let database = firebase.database();
 
-
-$('#favorite-button').on('click', function(){
-    database.ref().push({
-        artist: artist,
-        track: track,
-        trackid: trackId,
-    });
+$("#favorite-button").on("click", function() {
+  database.ref().push({
+    artist: artist,
+    track: track,
+    trackid: trackId
+  });
+  location.reload();
 });
 
-database.ref().on('child_added', function(childSnapshot){
-    console.log(childSnapshot.val());
-    let favArtist = childSnapshot.val().artist;
-    let favTrack = childSnapshot.val().track;
-    let key = childSnapshot.key;
-    let trackId = childSnapshot.val().trackid;
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+  let favArtist = childSnapshot.val().artist;
+  let favTrack = childSnapshot.val().track;
+  let key = childSnapshot.key;
+  let trackId = childSnapshot.val().trackid;
 
-    let favListItem = $('<li>');
-    let favTag = $('<a>');
-    favTag.addClass('f-button waves-effect waves-red btn-flat');
-    favTag.attr('data-artist', favArtist);
-    favTag.attr('data-id', trackId);
-    favTag.attr('data-track', favTrack);
-    favTag.attr('data-key', key);
-    favTag.text(favArtist +' - '+ favTrack);
-    favListItem.append(favTag);
+  let favListItem = $("<li>");
+  let favTag = $("<a>");
+  favTag.addClass("f-button waves-effect waves-red btn-flat");
+  favTag.attr("data-artist", favArtist);
+  favTag.attr("data-id", trackId);
+  favTag.attr("data-track", favTrack);
+  favTag.attr("data-key", key);
+  favTag.text(favArtist + " - " + favTrack);
+  favListItem.append(favTag);
 
-    $('#slide-out').append(favListItem);
-    // $('#favorites').append(favListItem);
+  $("#slide-out").append(favListItem);
+  // $('#favorites').append(favListItem);
 });
 
-$(document).on('click', '.favorites-button', function (event) {
-    artist = $(this).attr('data-artist');
-    track = $(this).attr('data-track');
-    $('#slide-out').hide();
-    $('#top-3').removeClass('hide');
-    $('#favorites-bar').addClass('hide');
-    $('#search-field').val("");
-    submitFunction(event);
-    counter = 0;
+$(document).on("click", ".favorites-button", function(event) {
+  artist = $(this).attr("data-artist");
+  track = $(this).attr("data-track");
+  $("#slide-out").hide();
+  $("#top-3").removeClass("hide");
+  $("#favorites-bar").addClass("hide");
+  $("#search-field").val("");
+  submitFunction(event);
+  counter = 0;
 });
 
-database.ref().on('child_added', function(childSnapshot){
-    console.log(childSnapshot.val());
-    let favArtist = childSnapshot.val().artist;
-    let favTrack = childSnapshot.val().track;
-    let key = childSnapshot.key;
-    let trackId = childSnapshot.val().trackid;
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+  let favArtist = childSnapshot.val().artist;
+  let favTrack = childSnapshot.val().track;
+  let key = childSnapshot.key;
+  let trackId = childSnapshot.val().trackid;
 
-    let favListItem = $('<li>');
-    let favTag = $('<a>');
-    favTag.addClass('favorites-button waves-effect waves-red btn-flat');
-    favTag.attr('data-artist', favArtist);
-    favTag.attr('data-id', trackId);
-    favTag.attr('data-track', favTrack);
-    favTag.attr('data-key', key);
-    favTag.text(favArtist +' - '+ favTrack);
-    favListItem.append(favTag);
+  let favListItem = $("<li>");
+  let favTag = $("<a>");
+  favTag.addClass("favorites-button waves-effect waves-red btn-flat");
+  favTag.attr("data-artist", favArtist);
+  favTag.attr("data-id", trackId);
+  favTag.attr("data-track", favTrack);
+  favTag.attr("data-key", key);
+  favTag.text(favArtist + " - " + favTrack);
+  favListItem.append(favTag);
 
-    // $('#slide-out').append(favListItem);
-    $('#favorites').append(favListItem);
+  // $('#slide-out').append(favListItem);
+  $("#favorites").append(favListItem);
 });
